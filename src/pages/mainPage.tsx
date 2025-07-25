3b872949c2e0d54bad32241f54be91fe1d91ed9f
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-07-25 10:35:55 +0500

    get items

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,7 +1,23 @@
+import axios from "axios";
+import { useEffect } from "react";
 
+// LocalStorage'dan tokenni olish
+const token = localStorage.getItem("token");
 
 export default function MainPage() {
-  return (
-    <div>MainPage</div>
-  )
+  useEffect(() => {
+    axios
+      .get(
+        `/api/reports/reports/materials?sort=name&start=1.07.2025&end=31.07.2025`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => console.log(res.data));
+  });
+
+  return <div>MainPage</div>;
 }
